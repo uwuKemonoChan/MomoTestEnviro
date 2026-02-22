caba033cf6ff59796721f73331b52e78223470c0
uwuKemonoChan <woodroyalty@gmail.com>
2026-02-21 22:38:37 -0600

    Wire vent crawl enter/exit interactions with do-afters

---
+++ Content.Shared/_DV/VentCrawling/Components/VentCrawlableComponent.cs
@@ -0,0 +1,23 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Maths;
+
+namespace Content.Shared._DV.VentCrawling.Components;
+
+/// <summary>
+/// Marks a vent/pipe node as crawlable and defines directional connectivity.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class VentCrawlableComponent : Component
+{
+    /// <summary>
+    /// Container that stores entities while traversing this node.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public string ContainerId = "VentCrawlingContainer";
+
+    /// <summary>
+    /// Directions that can be traversed from this node.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public Direction Connections = Direction.Invalid;
+}
